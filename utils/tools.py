cbcedf357fb69901075303adb46dbda4ac17d02b
tdkhoa1212 <tdkhoa1212@gmail.com>
2022-07-29 13:37:36 +0700

    mixup

--- utils/tools.py
+++ utils/tools.py
@@ -233,3 +233,27 @@ def matrices(y_true, y_pred):
     y_true = tf.cast(tf.math.argmax(y_true, axis=-1), dtype=tf.float32)
     acc = accuracy_score(y_true, y_pred)
     return acc, SE.numpy(), SP.numpy(), AS.numpy(), HS.numpy()
+
+##################################### MIXUP ###############################################################
+def sample_beta_distribution(size, concentration_0=0.2, concentration_1=0.2):
+    gamma_1_sample = tf.random.gamma(shape=[size], alpha=concentration_1)
+    gamma_2_sample = tf.random.gamma(shape=[size], alpha=concentration_0)
+    return gamma_1_sample / (gamma_1_sample + gamma_2_sample)
+
+
+def mix_up(ds_one, ds_two, alpha=0.2):
+    # Unpack two datasets
+    images_one, labels_one = ds_one
+    images_two, labels_two = ds_two
+    batch_size = tf.shape(images_one)[0]
+
+    # Sample lambda and reshape it to do the mixup
+    l = sample_beta_distribution(batch_size, alpha, alpha)
+    x_l = tf.reshape(l, (batch_size, 1, 1, 1))
+    y_l = tf.reshape(l, (batch_size, 1))
+
+    # Perform mixup on both images and labels by combining a pair of images/labels
+    # (one from each dataset) into one image/label
+    images = images_one * x_l + images_two * (1 - x_l)
+    labels = labels_one * y_l + labels_two * (1 - y_l)
+    return (images, labels)
