f612e79b44e76f9610c9f0bf7b262bcc8ea9f068
tdkhoa1212 <tdkhoa1212@gmail.com>
2022-07-30 16:54:01 +0700

    a

--- nets/CNN_1D_2D.py
+++ nets/CNN_1D_2D.py
@@ -1,5 +1,5 @@
 from nets.CNN_1D import cnn_1d_model
-from tensorflow import keras
+import tensorflow as tf
 from tensorflow.keras.layers import Conv1D, Activation, Dense, concatenate, BatchNormalization, GlobalAveragePooling1D, Input, MaxPooling1D, Lambda, GlobalAveragePooling2D, ReLU, MaxPooling2D, Flatten, Dropout, LSTM
 
 
