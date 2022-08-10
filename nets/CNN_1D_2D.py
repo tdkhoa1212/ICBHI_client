310e8e39ff7f91e5fb8fe62c9f2c4f4b4c527a2a
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-08-10 19:32:11 +0700

    Update CNN_1D_2D.py

--- nets/CNN_1D_2D.py
+++ nets/CNN_1D_2D.py
@@ -1,37 +1,63 @@
-from nets.CNN_1D import cnn_1d_model
-import tensorflow as tf
-from tensorflow.keras import Model, regularizers
 from tensorflow.keras.layers import Conv1D, Activation, Dense, concatenate, BatchNormalization, GlobalAveragePooling1D, Input, MaxPooling1D, Lambda, GlobalAveragePooling2D, ReLU, MaxPooling2D, Flatten, Dropout, LSTM
+import tensorflow as tf
+from keras.models import Model
+from tensorflow_addons.layers import MultiHeadAttention
+from keras import layers, regularizers
+import keras.backend as K
+
+def TransformerLayer(q, v, k, num_heads=4, training=None):
+    # Transformer layer https://arxiv.org/abs/2010.11929 (LayerNorm layers removed for better performance)
+    x1 = q
+    x2 = v
+    q = tf.keras.layers.Dense(128,   activation='relu',
+                                     kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+                                     bias_regularizer=regularizers.l2(1e-4),
+                                     activity_regularizer=regularizers.l2(1e-5))(q)
+    k = tf.keras.layers.Dense(128,   activation='relu',
+                                     kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+                                     bias_regularizer=regularizers.l2(1e-4),
+                                     activity_regularizer=regularizers.l2(1e-5))(k)
+    v = tf.keras.layers.Dense(128,   activation='relu',
+                                     kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+                                     bias_regularizer=regularizers.l2(1e-4),
+                                     activity_regularizer=regularizers.l2(1e-5))(v)
+    ma  = MultiHeadAttention(head_size=num_heads, num_heads=num_heads)([q, k, v])
+    ma = BatchNormalization()(ma, training=training)
+    ma = Activation('relu')(ma) 
+    
+    x1 = tf.keras.layers.Dense(128,   activation='relu',
+                                     kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+                                     bias_regularizer=regularizers.l2(1e-4),
+                                     activity_regularizer=regularizers.l2(1e-5))(x1)
+    x2 = tf.keras.layers.Dense(128,   activation='relu',
+                                     kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+                                     bias_regularizer=regularizers.l2(1e-4),
+                                     activity_regularizer=regularizers.l2(1e-5))(x2)
+    all_ = x1 + ma + x2
+    all_ = Dropout(0.1)(all_, training=training)
+    return all_
 
 
-def CNN_1D_2D_model(image_length=224, fft_length=64653, training=False):
-    ################# CNN 2D ################################
-    input_2D = Input(shape=(image_length, image_length, 1))
-    base_model_2D = tf.keras.applications.InceptionResNetV2(include_top=False,
-                                                            input_shape=(image_length, image_length, 1),
-                                                            weights=None)
-    model_2D = base_model_2D(input_2D, training=training)
-    output_2D = Model(input_2D, model_2D)
-    output_2D = output_2D([input_2D])
-    output_2D = GlobalAveragePooling2D()(output_2D)
-
-    ################# CNN 1D ################################
-    input_1D = Input(shape=(fft_length, 1))
-    base_model_1D = cnn_1d_model(fft_length, )
-    output_1D = base_model_1D([input_1D])
-
-
-    ################# CNN 1D vs 2D ################################
-    output = concatenate((output_1D, output_2D))
-    output = Dense(1024, activation=ReLU(), 
-                        kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-                        bias_regularizer=regularizers.l2(1e-4),
-                        activity_regularizer=regularizers.l2(1e-5))(output)
-    output = Dropout(0.1)(output, training=training)
-    output = Dense(4, activation='softmax', 
-                            kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-                            bias_regularizer=regularizers.l2(1e-4),
-                            activity_regularizer=regularizers.l2(1e-5))(output)
-
-    network = Model(inputs=[input_2D, input_1D], outputs=output)
-    return network
+def mix_model(opt, cnn_1d_model, resnet_50, lstm_extracted_model, input_1D, input_2D, input_extracted, training=False):
+  out_1D = cnn_1d_model(opt, training, input_1D)
+  out_2D = resnet_50(opt)(input_2D, training=training)
+  out_extracted = lstm_extracted_model(opt, training, input_extracted)
+  
+  network_1D = Model(input_1D, out_1D, name='network_1D')
+  network_2D = Model(input_2D, out_2D, name='network_2D')
+  network_extracted = Model(input_extracted, out_extracted, name='network_extracted')
+  
+  hidden_out_1D = network_1D([input_1D])
+  hidden_out_2D = network_2D([input_2D])
+  hidden_out_extracted = network_extracted([input_extracted])
+  
+  merged_value_0 = TransformerLayer(hidden_out_1D, hidden_out_2D, hidden_out_extracted, 4, training)
+  merged_value_1 = concatenate((hidden_out_1D, hidden_out_2D, hidden_out_extracted))
+    
+  Condition = Dense(3, 
+                    activation='softmax', 
+                    name='Condition')(merged_value_0)
+  RUL = Dense(1, 
+              activation='sigmoid', 
+              name='RUL')(merged_value_1)
+  return Condition, RUL
