b18bacacc0ec4b782d9f5dbc73b2d9da093f0a66
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-08-21 06:27:27 +0700

    Update CNN_1D_2D.py

--- nets/CNN_1D_2D.py
+++ nets/CNN_1D_2D.py
@@ -14,27 +14,27 @@ def CNN_1D_2D_model(image_length=224, fft_length=64653, training=False):
     output_2D = Model(input_2D, model_2D)
     output_2D = output_2D([input_2D])
     output_2D = GlobalAveragePooling2D()(output_2D)
-#     output_2D = Dropout(0.1)(output_2D, training=training)
     output_2D = Dense(1024, activation=ReLU(), 
                             kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                             bias_regularizer=regularizers.l2(1e-4),
                             activity_regularizer=regularizers.l2(1e-5))(output_2D)
+    output_2D = Dropout(0.1)(output_2D, training=training)
 
     ################# CNN 1D ################################
     input_1D = Input(shape=(fft_length, 1))
     base_model_1D = cnn_1d_model(fft_length, )
     output_1D = base_model_1D([input_1D])
-#     output_1D = Dropout(0.1)(output_1D, training=training)
-    output_1D = Dense(1024, activation=ReLU(), 
+    output_1D = Dense(256, activation=ReLU(), 
                             kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                             bias_regularizer=regularizers.l2(1e-4),
                             activity_regularizer=regularizers.l2(1e-5))(output_1D)
+    output_1D = Dropout(0.1)(output_1D, training=training)
 
     ################# CNN 1D vs 2D ################################
-    output = output_1D + output_2D
-    output = BatchNormalization()(output, training=training)
-    output = Activation('relu')(output)
-    output = Dropout(0.1)(output, training=training)
+#     output = output_1D + output_2D
+#     output = BatchNormalization()(output, training=training)
+#     output = Activation('relu')(output)
+#     output = Dropout(0.1)(output, training=training)
     output = concatenate((output_1D, output_2D))
     output = Dense(4, activation='softmax', 
                             kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
