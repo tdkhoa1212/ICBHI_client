fe0074dff3cecf6b40645dd57453d9e826d744b7
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-08-26 10:36:50 +0700

    Update CNN_1D_2D.py

--- nets/CNN_1D_2D.py
+++ nets/CNN_1D_2D.py
@@ -1,4 +1,4 @@
-from nets.CNN_1D import cnn_1d_model
+from nets.CNN_1D import cnn_1d_model, TransformerLayer
 import tensorflow as tf
 from tensorflow.keras import Model, regularizers
 from tensorflow.keras.layers import Conv1D, Activation, Dense, concatenate, BatchNormalization, GlobalAveragePooling1D, Input, MaxPooling1D, Lambda, GlobalAveragePooling2D, ReLU, MaxPooling2D, Flatten, Dropout, LSTM
@@ -13,7 +13,8 @@ def CNN_1D_2D_model(image_length=224, fft_length=64653, training=False):
     model_2D = base_model_2D(input_2D, training=training)
     output_2D = Model(input_2D, model_2D)
     output_2D = output_2D([input_2D])
-    output_2D = GlobalAveragePooling2D()(output_2D)
+    output_2D = TransformerLayer(output_2D, 1536, num_heads=8, training=training)
+#     output_2D = GlobalAveragePooling2D()(output_2D)
 #     output_2D = Dense(1024, activation=ReLU(), 
 #                             kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
 #                             bias_regularizer=regularizers.l2(1e-4),
@@ -31,10 +32,6 @@ def CNN_1D_2D_model(image_length=224, fft_length=64653, training=False):
 #     output_1D = Dropout(0.1)(output_1D, training=training)
 
     ################# CNN 1D vs 2D ################################
-    # output = output_1D + output_2D
-    # output = BatchNormalization()(output, training=training)
-    # output = Activation('relu')(output)
-    # output = Dropout(0.1)(output, training=training)
     output = concatenate((output_1D, output_2D))
     output = Dense(4, activation='softmax', 
                             kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
