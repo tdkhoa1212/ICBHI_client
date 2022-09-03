ca79638660cf49e173197b6a425823ba454db881
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-09-03 08:16:56 +0700

    Update CNN_1D.py

--- nets/CNN_1D.py
+++ nets/CNN_1D.py
@@ -26,7 +26,7 @@ def TransformerLayer(x, c, num_heads=16, training=None):
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(ma) 
-    ma = Dropout(0.15)(ma, training=training)
+#     ma = Dropout(0.15)(ma, training=training)
     ma = tf.keras.layers.Dense(c,  activation='relu',
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
@@ -118,7 +118,5 @@ def cnn_1d_model(input_shape, training=None):
 #     x = tf.keras.layers.Bidirectional(LSTM(units=512, return_sequences=False, activation='relu'))(x)
 #     x = Dropout(0.1)(x, training=training)
     x = TransformerLayer(x, 384, num_heads=16, training=training)
-#     x = tf.keras.layers.Bidirectional(GRU(units=384, return_sequences=False, activation='relu', unroll=True))(x)
-#     x = Dropout(0.15)(x, training=training)
     m_1 = Model(inputs, x)
     return m_1
